3eb288f4603883f88aa3ac8dc28045fa17f8803f
John Wu <webmaster@leapoahead.com>
2015-04-12 16:35:12 +0800

    Refactor shared components

--- UI/base/index.js
+++ UI/base/index.js
@@ -2,3 +2,11 @@
  * Require base CSS
  */
 require('./index.less');
+
+/**
+ * Render base modules that every page shares
+ */
+require(['components', 'react'], function(components, React) {
+  var Header = components.Header;
+  React.render(<Header />, document.getElementById('header'));
+});
--- UI/pages/home/index.js
+++ UI/pages/home/index.js
@@ -1,4 +1,3 @@
 require(['components', 'react'], function(components, React) {
-  var Header = components.Header;
-  React.render(<Header />, document.getElementById('header'));
+
 });
