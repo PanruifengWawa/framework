1237fa698eabe5070df5df9f7f0f2208da73385f
Esther <yaojingshuihudie@163.com>
2015-04-21 20:24:16 +0800

    。。。。。

--- app/Question.php
+++ app/Question.php
@@ -21,8 +21,5 @@ class Question extends Model {
   public function positions() {
     return $this->belongsToMany('App\Position');
   }
-<<<<<<< HEAD
-	
-=======
->>>>>>> 2c7f40b8d0424e6a92cc7b6e13b94486b87f1e94
+  
 }
