941ea3e5915157eab192fe991774e9da4a09bdbb
John Wu <webmaster@leapoahead.com>
2015-04-21 14:20:17 +0800

    Database seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,9 +12,27 @@ class DatabaseSeeder extends Seeder {
 	 */
 	public function run()
 	{
-		Model::unguard();
+		DB::table('users')->delete();
+		DB::table('questions')->delete();
 
-		// $this->call('UserTableSeeder');
+		$user = App\User::create(array(
+			'email' => 'test1@interu.com',
+			'name' => 'John Wu',
+			'password' => 'e10adc3949ba59abbe56e057f20f883e',
+			'avatar' => 'http://placehold.it/80x80'
+		));
+
+		$question[0] = App\Question::create(array(
+			'user_id' => $user->id,
+			'content' => '请问在Backbone.js中怎么创建一个Model？'
+		));
+
+		$question[1] = App\Question::create(array(
+			'user_id' => $user->id,
+			'content' => '请问在Angular.js中怎么创建一个Service？'
+		));
+
+
 	}
 
 }
