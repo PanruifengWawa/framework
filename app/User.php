03e53226c5eb37c635509eb76e8dcb8b30bfd3f2
John Wu <webmaster@leapoahead.com>
2015-04-21 14:09:50 +0800

    User fill guard

--- app/User.php
+++ app/User.php
@@ -3,6 +3,11 @@
 use Illuminate\Database\Eloquent\Model;
 
 class User extends Model {
+  protected $fillable = array('name',
+                              'email',
+                              'password',
+                              'avatar');
+
   public function questions() {
     return $this->hasMany('App\Question');
   }
