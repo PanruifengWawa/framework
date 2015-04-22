7735e0f285397790c20b1704f9f019585a760ba1
John Wu <webmaster@leapoahead.com>
2015-04-23 07:14:34 +0800

    fixed #17

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,10 +3,19 @@
 <head>
 	<meta charset="UTF-8">
 	<title>InterU</title>
+	<?php
+		$user = \Session::get('user');
+		if(!$user)
+			$user = 'undefined';
+		else{
+			unset($user->password);
+			$user = $user->toJson();
+		}
+	?>
 	<script>
 		window.iu = {};
-		window.iu['token'] = '{{csrf_token()}}';
-		// window.iu['user'] = @{{$user}}};
+		window.iu['token'] = '{{{csrf_token()}}}';
+		window.iu['user'] = <?php echo $user ?>;
 	</script>
 </head>
 <body>
