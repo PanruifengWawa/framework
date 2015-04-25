24733e0782795df7c62388ea1115d43d83bec6ed
wpexia <wpexia@gmail.com>
2015-04-25 10:47:15 +0800

    change config

--- tests/WelcomeControllerTest.php
+++ tests/WelcomeControllerTest.php
@@ -6,9 +6,12 @@ class WlecomeControllerTest extends TestCase {
 	public function testIndex()
 	{
 		Session::start();
-		Session::set('user',\App\User::all()->first());
+		Session::set('user',$user = \App\User::all()->first());
+		$questions = \App\Question::where('user_id', '=', $user->id)->paginate(20);
 		$response = $this->call('GET','/',[]);
 
-		
+		$this->assertResponseStatus(200);
+		$this->assertViewHas("questions", $questions);
+		$this->assertViewHas("user", $user);
 	}
 }
