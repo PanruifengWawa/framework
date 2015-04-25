7af32ae3348309e56b4533a1430a09cb24408662
wawa <46937922@qq.com>
2015-04-25 14:18:19 +0800

    fix

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -10,6 +10,49 @@ use App\User;
 
 class QuestionController extends Controller {
 
+    public function store(){
+        $company = new Company();
+        $company_name = \Request::input('company_name');
+        if(!$company_name)return $this->reportError("公司名称不能为空");
+        try {
+            $company = Company::where('name', '=', $company_name)->firstOrFail();
+        }catch (ModelNotFoundException $e){
+            return $this->reportError("公司不存在");
+        }
+        $position = new Position();
+        $position_name = \Request::input('position_name');
+        if(!$position_name)return $this->reportError("职位名称不能为空");
+        try {
+            $position = Position::where('title', '=', $position_name)->firstOrFail();
+        }catch (ModelNotFoundException $e){
+            return $this->reportError("职位不存在");
+        }
+        $user = new User();
+        $user_id = \Request::input('user_id');
+        if(!$user_id)return $this->reportError("用户名不能为空");
+        try {
+            $user = User::find($user_id)->firstOrFail();
+        } catch(ModelNotFoundException $e){
+            return $this->reportError("用户不存在");
+        }
+        $questions = \Request::input('questions');
+        if(!$questions)return $this->reportError("问题不能为空");
+        $de_questions = json_decode($questions);
+        $count_ques = count($de_questions);
+        $question_ids = array();
+        for($i = 0; $i < $count_ques; $i++)
+        {
+            $question = new Question();
+            $question->content = $de_questions[$i];
+            $question->user_id = $user_id;
+            $question->save();
+            array_push($question_ids, $question->id);
+            $question->companies()->attach($company->id);
+            $question->positions()->attach($position->id);
+        }
+        return \Response::make(json_encode($question_ids), 200);
+    }
+
     public function show($questionId){
         if (!\Request::isMethod('get')) {
             return $this->reportError("非法请求");
@@ -85,9 +128,3 @@ class QuestionController extends Controller {
         return $response;
     }
 }
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2015/3/30
- * Time: 15:43
- */
