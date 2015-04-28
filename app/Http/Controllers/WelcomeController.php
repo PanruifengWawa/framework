1aaa19a598595d346875a25c9e0b6803793e8f01
John Wu <webmaster@leapoahead.com>
2015-04-28 18:26:05 +0800

    Questions desc

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -8,7 +8,10 @@ class WelcomeController extends Controller {
 	{
 		$user = \Session::get('user');
 		if ( $user ) {
-			$questions = Question::with(['companies', 'comments'])->where('user_id', '=', $user->id)->paginate(20);
+			$questions = Question::with(['companies', 'comments'])
+				->where('user_id', '=', $user->id)
+				->orderBy('created_at', 'desc')
+				->paginate(20);
 			return view('home', [
 					'questions' => $questions,
 					'user' => $user
