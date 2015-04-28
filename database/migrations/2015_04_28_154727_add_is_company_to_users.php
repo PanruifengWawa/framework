730d9e58ade981c1cbf61637aded0b73b6beaea9
John Wu <webmaster@leapoahead.com>
2015-04-29 00:00:25 +0800

    combine companies, admins and users table

---
+++ database/migrations/2015_04_28_154727_add_is_company_to_users.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIsCompanyToUsers extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			$table->boolean('is_company')->default(false);
+			$table->integer('company_id')
+				->unsigned()
+				->nullable();
+			$table->foreign('company_id')
+				->references('id')
+				->on('companies')
+				->onDelete('cascade');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			$table->dropColumn(['is_company']);
+		});
+	}
+
+}
