bbb2c43d3da0470be5b1b061724e379467132c94
John Wu <webmaster@leapoahead.com>
2015-04-28 08:20:12 +0800

    Company controller test

---
+++ tests/CompanyControllerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+
+class CompanyControllerTest extends TestCase {
+
+    public function testIndexWhenNotKeywordIsPresented()
+    {
+        $response = $this->call('GET', '/companies');
+
+        $body = json_decode($response->getContent(), true);
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals($body[0]['name'], 'IBM');
+    }
+}
